03de1e452164a48534d0026e8f3566197b229eb3
Izabelle Cesario <izabellecesario@hotmail.com>
2023-08-31 15:48:52 -0300

    ap01(java): hello HelloWorld
    - Utilizando a linha de comando

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    //classes sempre iniciam o nome com letra maiúscula
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
+//Java o compitaldor porduz bytecode, linguagem que a JVM entende.
