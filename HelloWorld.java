b5dcf24cd592948e62c65a2d1bda57baa68966df
Izabelle Cesario <izabellecesario@hotmail.com>
2023-08-31 16:04:58 -0300

    ap01(java): hello world
    - Utilizando JOptionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
+//pacote padrao: java.lang... importado automaticamente
+import javax.swing.JOptionPane;
+
 public class HelloWorld{
     //classes sempre iniciam o nome com letra maiúscula
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+      // System.out.println("Hello, World!");
+      JOptionPane.showMessageDialog(null,"Hello, World!");
     }
 }
-//Java o compitaldor porduz bytecode, linguagem que a JVM entende.
+//Java o compitalador porduz bytecode, linguagem que a JVM entende.
